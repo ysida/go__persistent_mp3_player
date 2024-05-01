38f68d23640018a4ac838bfbb22f174183625311
x <x@live.com>
2024-05-01 08:27:46 +0300

    as is

--- main.go
+++ main.go
@@ -7,6 +7,27 @@ import (
 	"time"
 )
 
+func getMp3Files() []string {
+	files, err := os.ReadDir(".")
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return nil
+	}
+
+	var mp3Files []string
+	for _, file := range files {
+		if !file.IsDir() && isMP3File(file.Name()) {
+			mp3Files = append(mp3Files, file.Name())
+		}
+	}
+	return mp3Files
+}
+
+func isMP3File(filename string) bool {
+	return len(filename) > 4 && filename[len(filename)-4:] == ".mp3"
+}
+
+
 func playMP3Files() {
 	files, err := os.ReadDir(".")
 	if err != nil {
@@ -38,9 +59,6 @@ func playMP3Files() {
 	}
 }
 
-func isMP3File(filename string) bool {
-	return len(filename) > 4 && filename[len(filename)-4:] == ".mp3"
-}
 
 func main() {
 	playMP3Files()
