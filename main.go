e5c845035de31a7c88fcc13cc6e89522846f1133
x <x@live.com>
2024-05-01 08:25:50 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func playMP3Files() {
+	files, err := os.ReadDir(".")
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return
+	}
+
+	var currentPlaying int
+	for {
+		if currentPlaying >= len(files) {
+			currentPlaying = 0
+		}
+
+		file := files[currentPlaying]
+		if file.IsDir() || !isMP3File(file.Name()) {
+			currentPlaying++
+			continue
+		}
+
+		fmt.Println("Playing:", file.Name())
+		cmd := exec.Command("mpg123", file.Name())
+		cmd.Run()
+
+		// Persist current playing location every 5 seconds
+		time.Sleep(5 * time.Second)
+		fmt.Println("Persisting current playing location...")
+
+		currentPlaying++
+	}
+}
+
+func isMP3File(filename string) bool {
+	return len(filename) > 4 && filename[len(filename)-4:] == ".mp3"
+}
+
+func main() {
+	playMP3Files()
+}
